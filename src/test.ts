bc86feb45b27922ec3237c8095b067fdd5004095
Simon Chan <cnsimonchan@live.com>
2020-05-26 18:15:29 +0800

    feat(shell): support search

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,5 @@
 import { Terminal } from 'xterm';
+import { SearchAddon } from 'xterm-addon-search';
 import { WebAdb } from './webadb.js';
 import 'xterm/css/xterm.css';
 
@@ -14,7 +15,27 @@ document.getElementById('start')!.onclick = async () => {
     const shell = await adb.shell();
 
     const terminal = new Terminal({
+        scrollback: 9001,
     });
+
+    const searchAddon = new SearchAddon();
+    terminal.loadAddon(searchAddon);
+
+    const keyword = document.getElementById('search-keyword')! as HTMLInputElement;
+    keyword.addEventListener('input', () => {
+        searchAddon.findNext(keyword.value, { incremental: true });
+    });
+
+    const next = document.getElementById('search-next')!;
+    next.addEventListener('click', () => {
+        searchAddon.findNext(keyword.value);
+    });
+
+    const prev = document.getElementById('search-prev')!;
+    prev.addEventListener('click', () => {
+        searchAddon.findPrevious(keyword.value);
+    });
+
     terminal.open(document.getElementById('terminal')!);
     terminal.onData(data => {
         const { buffer } = textEncoder.encode(data);
