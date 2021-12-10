3260dfe72c4f0283b046d6e6c7466b0a4d6d8630
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2021-12-10 12:38:25 +0800

    chore: configure base path

--- apps/demo/next.config.js
+++ apps/demo/next.config.js
@@ -14,6 +14,7 @@ const withMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 module.exports = withMDX({
+    basePath: '/ya-webadb',
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
     reactStrictMode: true,
     productionBrowserSourceMaps: true,
