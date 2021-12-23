e219687ea6a72c46bcd37629870b76960d1685bb
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2021-12-23 10:34:41 +0800

    ci: add vercel ignore build script

---
+++ common/scripts/vercel-should-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "gh-pages" ]] ; then
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+else
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+fi
