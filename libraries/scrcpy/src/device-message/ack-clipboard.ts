a15498f34d461dfa5b043f7f061f55ce1812db20
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2023-01-20 21:03:51 +0800

    fix(scrcpy): fix message type

--- libraries/scrcpy/src/device-message/ack-clipboard.ts
+++ libraries/scrcpy/src/device-message/ack-clipboard.ts
@@ -4,7 +4,7 @@ import { ScrcpyDeviceMessageType } from "./type.js";
 
 export const ScrcpyAckClipboardDeviceMessage = new Struct()
     .uint64("sequence")
-    .extra({ type: ScrcpyDeviceMessageType.Clipboard as const });
+    .extra({ type: ScrcpyDeviceMessageType.AckClipboard as const });
 
 export type ScrcpyAckClipboardDeviceMessage =
     typeof ScrcpyAckClipboardDeviceMessage["TDeserializeResult"];
