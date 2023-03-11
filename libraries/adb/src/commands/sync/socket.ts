190b24ad5ecdf9d134138d28d95d0ce21c472a77
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2023-03-11 13:18:00 +0800

    refactor(stream): add `BufferCombiner` for more flexible uses

--- libraries/adb/src/commands/sync/socket.ts
+++ libraries/adb/src/commands/sync/socket.ts
@@ -1,5 +1,9 @@
 import type { WritableStreamDefaultWriter } from "@yume-chan/stream-extra";
-import { BufferedReadableStream, Consumable } from "@yume-chan/stream-extra";
+import {
+    BufferCombiner,
+    BufferedReadableStream,
+    Consumable,
+} from "@yume-chan/stream-extra";
 import type { StructAsyncDeserializeStream } from "@yume-chan/struct";
 
 import type { AdbSocket } from "../../index.js";
@@ -10,12 +14,9 @@ export class AdbSyncSocketLocked implements StructAsyncDeserializeStream {
         Consumable<Uint8Array>
     >;
     private readonly _readable: BufferedReadableStream;
-    private readonly _bufferCapacity: number;
     private readonly _socketLock: AutoResetEvent;
     private readonly _writeLock = new AutoResetEvent();
-    private readonly _writeBuffer: Uint8Array;
-    private _writeBufferOffset = 0;
-    private _writeBufferAvailable;
+    private readonly _combiner: BufferCombiner;
 
     public constructor(
         writer: WritableStreamDefaultWriter<Consumable<Uint8Array>>,
@@ -25,10 +26,8 @@ export class AdbSyncSocketLocked implements StructAsyncDeserializeStream {
     ) {
         this._writer = writer;
         this._readable = readable;
-        this._bufferCapacity = bufferSize;
         this._socketLock = lock;
-        this._writeBuffer = new Uint8Array(bufferSize);
-        this._writeBufferAvailable = bufferSize;
+        this._combiner = new BufferCombiner(bufferSize);
     }
 
     private async writeInnerStream(buffer: Uint8Array) {
@@ -40,15 +39,10 @@ export class AdbSyncSocketLocked implements StructAsyncDeserializeStream {
     public async flush() {
         try {
             await this._writeLock.wait();
-            if (this._writeBufferOffset === 0) {
-                return;
+            const buffer = this._combiner.flush();
+            if (buffer) {
+                await this.writeInnerStream(buffer);
             }
-
-            await this.writeInnerStream(
-                this._writeBuffer.subarray(0, this._writeBufferOffset)
-            );
-            this._writeBufferOffset = 0;
-            this._writeBufferAvailable = this._bufferCapacity;
         } finally {
             this._writeLock.notifyOne();
         }
@@ -57,46 +51,8 @@ export class AdbSyncSocketLocked implements StructAsyncDeserializeStream {
     public async write(data: Uint8Array) {
         try {
             await this._writeLock.wait();
-            let offset = 0;
-            let available = data.byteLength;
-            if (this._writeBufferOffset !== 0) {
-                if (available >= this._writeBufferAvailable) {
-                    this._writeBuffer.set(
-                        data.subarray(0, this._writeBufferAvailable),
-                        this._writeBufferOffset
-                    );
-                    offset += this._writeBufferAvailable;
-                    available -= this._writeBufferAvailable;
-
-                    await this.writeInnerStream(this._writeBuffer);
-                    this._writeBufferOffset = 0;
-                    this._writeBufferAvailable = this._bufferCapacity;
-
-                    if (available === 0) {
-                        return;
-                    }
-                } else {
-                    this._writeBuffer.set(data, this._writeBufferOffset);
-                    this._writeBufferOffset += available;
-                    this._writeBufferAvailable -= available;
-                    return;
-                }
-            }
-
-            while (available >= this._bufferCapacity) {
-                const end = offset + this._bufferCapacity;
-                await this.writeInnerStream(data.subarray(offset, end));
-                offset = end;
-                available -= this._bufferCapacity;
-            }
-
-            if (available > 0) {
-                this._writeBuffer.set(
-                    data.subarray(offset),
-                    this._writeBufferOffset
-                );
-                this._writeBufferOffset += available;
-                this._writeBufferAvailable -= available;
+            for (const buffer of this._combiner.push(data)) {
+                await this.writeInnerStream(buffer);
             }
         } finally {
             this._writeLock.notifyOne();
@@ -109,8 +65,7 @@ export class AdbSyncSocketLocked implements StructAsyncDeserializeStream {
     }
 
     public release(): void {
-        this._writeBufferOffset = 0;
-        this._writeBufferAvailable = this._bufferCapacity;
+        this._combiner.flush();
         this._socketLock.notifyOne();
     }
 }
