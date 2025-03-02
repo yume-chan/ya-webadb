a335c1495c54d4d5334ccaff0bbb4c9c756d56a8
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2025-03-02 21:25:16 +0800

    feat(stream): improve SplitStringStream handling incomplete chunks

--- libraries/stream-extra/src/split-string.ts
+++ libraries/stream-extra/src/split-string.ts
@@ -1,30 +1,31 @@
 import { TransformStream } from "./stream.js";
 
-function* split(
-    input: string,
-    separator: string,
-): Generator<string, void, void> {
-    let start = 0;
-
-    while (true) {
-        const index = input.indexOf(separator, start);
-        if (index === -1) {
-            return;
-        }
-
-        const part = input.substring(start, index);
-        yield part;
-
-        start = index + 1;
-    }
-}
-
 export class SplitStringStream extends TransformStream<string, string> {
     constructor(separator: string) {
+        let remaining: string | undefined = undefined;
+
         super({
             transform(chunk, controller) {
-                for (const part of split(chunk, separator)) {
-                    controller.enqueue(part);
+                if (remaining) {
+                    chunk = remaining + chunk;
+                    remaining = undefined;
+                }
+
+                let start = 0;
+                while (start < chunk.length) {
+                    const index = chunk.indexOf(separator, start);
+                    if (index === -1) {
+                        remaining = chunk.substring(start);
+                        break;
+                    }
+
+                    controller.enqueue(chunk.substring(start, index));
+                    start = index + 1;
+                }
+            },
+            flush(controller) {
+                if (remaining) {
+                    controller.enqueue(remaining);
                 }
             },
         });
