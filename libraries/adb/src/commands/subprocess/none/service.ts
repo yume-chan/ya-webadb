1a769d9f805fc388418c4f2095bb0eeecd64d547
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2025-09-15 15:00:09 +0800

    refactor(adb): cleanup subprocess service string building

--- libraries/adb/src/commands/subprocess/none/service.ts
+++ libraries/adb/src/commands/subprocess/none/service.ts
@@ -17,15 +17,20 @@ export class AdbNoneProtocolSubprocessService {
     spawn = adbNoneProtocolSpawner(async (command, signal) => {
         // Android 7 added `shell,raw:${command}` service which also triggers raw mode,
         // but we want to use the most compatible one.
-        //
+        let service = "exec:";
+
+        if (!command.length) {
+            throw new Error("Command cannot be empty");
+        }
+
         // Similar to SSH, we don't escape the `command`,
         // because the command will be invoked by `sh -c`,
         // it can contain environment variables (`KEY=value command`),
         // and shell expansions (`echo "$KEY"` vs `echo '$KEY'`),
         // which we can't know how to properly escape.
-        const socket = await this.#adb.createSocket(
-            `exec:${command.join(" ")}`,
-        );
+        service += command.join(" ");
+
+        const socket = await this.#adb.createSocket(service);
 
         if (signal?.aborted) {
             await socket.close();
@@ -38,17 +43,17 @@ export class AdbNoneProtocolSubprocessService {
     async pty(
         command?: string | readonly string[],
     ): Promise<AdbNoneProtocolPtyProcess> {
-        if (command === undefined) {
-            // Run the default shell
-            command = "";
+        let service = "shell:";
+
+        if (typeof command === "string") {
+            service += command;
         } else if (Array.isArray(command)) {
             // Don't escape `command`. See `spawn` above for details
-            command = command.join(" ");
+            service += command.join(" ");
         }
 
         return new AdbNoneProtocolPtyProcess(
-            // https://github.com/microsoft/typescript/issues/17002
-            await this.#adb.createSocket(`shell:${command as string}`),
+            await this.#adb.createSocket(service),
         );
     }
 }
