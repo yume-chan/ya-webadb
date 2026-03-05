d1e2e20aa413a600bf45eb8a081d8aba88d4d034
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2026-03-05 23:49:58 +0800

    ci: just use absolute path for annotations

--- libraries/adb/src/fail.spec.ts
+++
@@ -1,8 +0,0 @@
-import assert from "node:assert";
-import { describe, it } from "node:test";
-
-describe("failed test", () => {
-    it("will fail", () => {
-        assert.deepStrictEqual({ foo: 1 }, { bar: true });
-    });
-});
