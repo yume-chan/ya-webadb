2fc7cd5ba8d4c98eca264fea0496727e1aa920de
Lexip <x@lexip.dev>
2026-07-27 15:41:28 +0200

    style(webusb): Fix typo in webusb error message (#855)

--- libraries/adb-daemon-webusb/src/error.ts
+++ libraries/adb-daemon-webusb/src/error.ts
@@ -1,6 +1,6 @@
 export class DeviceBusyError extends Error {
     constructor(cause?: Error) {
-        super("The device is already in used by another program", {
+        super("The device is already in use by another program", {
             cause,
         });
     }
